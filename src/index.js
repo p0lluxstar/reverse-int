798ce3dd4c85e543335a2417dfe26a92c9760d95
global-p0lluxstar <global-web-registra@yandex.ru>
2023-02-15 00:35:12 +0300

    150223-00:34

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
+    let arr = [];
+    let rever = '';
   
-}
+    for (let i = 0; i < String(n).length; i++){
+      if(Number(String(n)[i]) >= 0 ){
+          arr[i] = String(n)[i];
+      };
+    };
+  
+    arr.reverse().forEach(el => { //метод reverse() позволяет обратить порядок следования элементов массива
+      rever += el;
+    });
+
+    return Number(rever);
+};
